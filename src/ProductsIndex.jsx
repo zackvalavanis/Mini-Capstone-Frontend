fe2e56a01527833353f171f1de1e38d007ad771e
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-25 19:56:28 -0500

    index action

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,9 +1,15 @@
-export function ProductsIndex (props) { 
+export function ProductsIndex ({products}) { 
   return ( 
-    <h1>All Products</h1>>
-      <div>
-        <h1>Name: {products.name}</h1>
-      </div>
-  
-  );
+    <div>
+      <h1>All Products!</h1>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>Name: {product.name}</h2>
+          <h2>Price: {product.price}</h2>
+          <img src={product.images[0].image_url} />
+        </div>
+      ))}
+    </div>
+  )
 }
+
--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,21 +1,25 @@
 import { ProductsIndex } from './ProductsIndex'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useState, useEffect } from 'rails'
 
 export function ProductsPage () { 
 
-  const [ Products, setProducts ] = useState([]); 
+  const [ products, setProducts ] = useState([]); 
 
   const handleIndex = () => { 
-    
-  }
+    console.log('handleIndex');
+    axios.get('http://localhost:3000/products.json').then((response) => { 
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  };
+
+useEffect( handleIndex, [] );
 
 
   return ( 
     <main>
-      <div>
-        <ProductsIndex />
-      </div>
+      <ProductsIndex products={products}/>
     </main>
   )
 }
