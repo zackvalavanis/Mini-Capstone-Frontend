5beade3e554f85e60d0e5b38dd01d68008dfd6dc
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-27 11:31:11 -0500

    added multiple images

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -6,13 +6,13 @@ export function ProductsIndex({ products, onShow }) {
         <div key={product.id}>
           <h2>Name: {product.name}</h2>
           <h2>Price: {product.price}</h2>
-          {product.images && product.images.length > 0 && ( 
-          <img 
-            className='images'
-            src={product.images[0].image_url}
-            alt={product.name}
-          /> 
-          )}
+          {/* Check if images exist before trying to render */}
+          {product.images[0] && <img src={product.images[0].image_url} alt={product.name} />}
+          
+          {/* Render images_with_default with the correct image reference */}
+          {product.images_with_default.map((image) => (
+            <img key={image.id} src={image.image_url} alt="Product Image" />
+          ))}
           <button onClick={() => onShow(product)}>More Info</button>
         </div>
       ))}
