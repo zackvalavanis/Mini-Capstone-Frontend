98d4d28655761d9a41b9ea9df6984f7fa3c2d5c6
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-06 16:45:32 -0500

    first bootstrap additions

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,101 @@ import { ProductsNewPage } from './ProductsNewPage';
 import { SignupPage } from './SignupPage';
 import { LoginPage } from './LoginPage';
 import { ProductsIndexPage } from './ProductsIndexPage'
+// import { ProductsShowPage } from './ProductsShowPage'
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { LogoutLink } from './LogoutLink'
+
 
 
 const router = createBrowserRouter([
   {
     element: ( 
       <div>
-       <Header />
+        <div>
+          <Header />
+          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">Navbar</a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Link
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Dropdown
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <a className="dropdown-item" href="/LoginPage">
+                          Login
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="/SignupPage">
+                          Signup
+                        </a>
+                      </li>
+                      <li>
+                        <hr className="dropdown-divider" />
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="/products/new">
+                          New Product
+                        </a>
+                      </li>
+                      <li>
+                        <LogoutLink>Logout</LogoutLink>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link disabled" aria-disabled="true">
+                      Disabled
+                    </a>
+                  </li>
+                </ul>
+                <form className="d-flex" role="search">
+                  <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <button className="btn btn-outline-success" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        </div>
        <Outlet />
        <Footer />
       </div>
@@ -40,6 +127,11 @@ const router = createBrowserRouter([
         element: <ProductsIndexPage />, 
         loader: () => axios.get('http://localhost:3000/products.json').then(response => response.data)
       }
+      // { 
+      //   path: './products/:id',
+      //   element: <ProductsShowPage />, 
+      //   loader: () => axios.get
+      // }
     ],
   },
 ]);
