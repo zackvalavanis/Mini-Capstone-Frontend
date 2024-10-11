9ed1cd4a08ad324e47564dd447980d80ec3e42ca
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-10 20:15:05 -0500

    working on index for orders

--- src/OrdersIndex.jsx
+++ src/OrdersIndex.jsx
@@ -9,7 +9,17 @@ export function OrdersIndex () {
       {orders.map((order) => (
         <div key={order.id}>
           <h1>Order Number: {order.id}</h1>
-          <p>{order.products[0] && order.products[0].name}.</p>  
+          {order.products.map((product) => (
+            <div key={product.id}>
+              <p>{product.name}.</p>  
+              {order.carted_products.map((carted_product) => { 
+                <div key={carted_product.id}>
+                  <h1>{carted_product.quantity}</h1>
+                </div>
+              })}
+              <p> Quantity: {order.quantity}</p>
+            </div>
+          ))}
           <p>Subtotal: {order.subtotal}</p>
           <p>Sales Tax: {order.tax}</p>
           <p>Total: {order.total}</p>
