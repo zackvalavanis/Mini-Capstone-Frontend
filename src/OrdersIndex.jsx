23c46035bf0463b743792a49f485c5a9170ed50c
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-10 20:47:54 -0500

    order index page

--- src/OrdersIndex.jsx
+++ src/OrdersIndex.jsx
@@ -6,31 +6,27 @@ export function OrdersIndex () {
   return (
     <div>
       <h1>All Orders</h1>
-      {orders.map((order) => (
-        <div key={order.id}>
-          <h1>Order Number: {order.id}</h1>
-          {order.products.map((product) => (
-            <div key={product.id}>
-              <p>{product.name}.</p>  
-              {order.carted_products.map((carted_product) => { 
-                <div key={carted_product.id}>
-                  <h1>{carted_product.quantity}</h1>
-                </div>
-              })}
-              <p> Quantity: {order.quantity}</p>
+      {(!orders || orders.length === 0) ? (
+        <div>No orders available.</div>
+      ) : (
+        <>
+          {orders[0].carted_products.map((cp) => (
+            <div key={cp.id}>
+              <p>{cp.product_name} Quantity: {cp.quantity} -- Price: {cp.product_price}</p>
             </div>
           ))}
-          <p>Subtotal: {order.subtotal}</p>
-          <p>Sales Tax: {order.tax}</p>
-          <p>Total: {order.total}</p>
-        </div>
-      ))}
-
-      
+          {orders.map(({ id, subtotal, tax, total }) => (
+            <div key={id}>
+              <p>Subtotal: {subtotal}</p>
+              <p>Tax: {tax}</p>
+              <p>Total: {total}</p>
+            </div>
+          ))}
+        </>
+      )}
     </div>
-  )
+  );
 }
-
 
 // "subtotal":
 // "tax"
